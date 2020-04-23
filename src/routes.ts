2bf40ff6896a4b21a5c34da36d0d9f9475422cc8
Zachary Shute <zach@opentable.com>
2020-04-23 14:58:42 -0700

    add pause merging button

--- src/routes.ts
+++ src/routes.ts
@@ -13,18 +13,22 @@ declare global {
 
 export function routes(state: State, root: string, router: Router): void {
   router.get("/", (_req, res) => {
-    const { enabled, proposedTrain, decisionLog } = state;
+    const { enabled, mergingEnabled, proposedTrain, decisionLog } = state;
     res.send(`
 <html>
 <body>
 <pre>
 proposed PRs: ${JSON.stringify(proposedTrain)}
 enabled: ${enabled ? "yes" : "no"}
+merging: ${mergingEnabled ? "enabled" : "disabled"}
 queue length: ${state.queue.size}
 lru cache size: ${state.cacheState.lru.size}
 rate limit remaining: ${state.cacheState.remainingLimit} out of ${state.cacheState.rateLimit}
 </pre>
-<form method="post" action="${root}"><button>Turn ${!enabled ? "on" : "off"}</button></form>
+<form method="post" action="${root}"><button name="enabled">Turn ${!enabled ? "on" : "off"}</button></form>
+<form method="post" action="${root}"><button name="mergingEnabled">${
+      !mergingEnabled ? "Unpause Merging" : "Pause Merging"
+    }</button></form>
 <pre>Log:
 ${decisionLog.join("\n")}
 </pre>
@@ -34,8 +38,16 @@ ${decisionLog.join("\n")}
   });
 
   router.post("/", (req, res) => {
-    state.enabled = !state.enabled;
-    req.log(`changing enabled state to ${state.enabled}`);
-    res.redirect(root);
+    const bodyChunks: Uint8Array[] = [];
+    req
+      .on("data", (chunk) => {
+        bodyChunks.push(chunk);
+      })
+      .on("end", () => {
+        const stateValue = Buffer.concat(bodyChunks).toString("utf8").split("=")[0];
+        state[stateValue] = !state[stateValue];
+        req.log(`changing ${stateValue} state to ${state[stateValue]}`);
+        res.redirect(root);
+      });
   });
 }
--- src/state.ts
+++ src/state.ts
@@ -1,10 +1,11 @@
 import PQueue from "p-queue";
 import { CacheState } from "./cache";
-
 export interface State {
   proposedTrain: Array<number>;
   enabled: boolean;
+  mergingEnabled: boolean;
   decisionLog: Array<string>;
   queue: PQueue;
   cacheState: CacheState;
+  [key: string]: boolean | CacheState | PQueue | Array<string> | Array<number>;
 }
